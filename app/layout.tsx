8c0b4144845923da0d413a363dd827dd3f95073f
mjstoney <77265862+mjstoney@users.noreply.github.com>
2023-08-29 15:46:37 -0700

    Fix layout background

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="bg-gray-700 min-h-screen flex flex-col justify-between items-center">
+        <div className="bg-gray-200 min-h-screen flex flex-col justify-between items-center">
           <header className="">
             <Navbar />
           </header>
